73166bfd49f3285763641073a59f6cf53e82a516
momommx <106795266+momommx@users.noreply.github.com>
2022-07-01 12:02:40 +0000

    応用課題：課題4

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,10 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :following_user, through: :follower, source: :followed
+  has_many :follower_user, through: :followed, source: :follower
 
   has_one_attached :profile_image, dependent: :destroy
 
@@ -20,5 +24,20 @@ class User < ApplicationRecord
       profile_image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+  
+  # ユーザーをフォローする
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+
+  # ユーザーのフォローを外す
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+
+  # フォローしていればtrueを返す
+  def following?(user)
+    following_user.include?(user)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,22 @@
 Rails.application.routes.draw do
+  get 'relationships/create'
+  get 'relationships/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: 'homes#top'
   get "home/about"=>"homes#about"
   
-  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+  resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
    resources :book_comments, only: [:create, :destroy]
    resource :favorites, only: [:create, :destroy]
   end
    
-  resources :users, only: [:index,:show,:edit,:update]
-  
+  resources :users, only: [:index, :show, :edit, :update] do
+      member do
+        get :following, :followers
+      end
+      resources :relationships, only: [:create, :destroy]
+  end
+    
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
