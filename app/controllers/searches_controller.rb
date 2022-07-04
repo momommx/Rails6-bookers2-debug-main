725860b8f488241f052a5d918e04476ca78fddb6
momommx <106795266+momommx@users.noreply.github.com>
2022-07-04 15:02:52 +0000

    応用課題：課題5_検索機能

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,6 +2,9 @@ class SearchesController < ApplicationController
   before_action :authenticate_user!
 
   def search
+    @users = User.all
+    @books = Book.all
+    @word = params[:word]
     @range = params[:range]
 
     if @range == "User"
--- app/models/user.rb
+++ app/models/user.rb
@@ -40,4 +40,19 @@ class User < ApplicationRecord
   def following?(user)
     following_users.include?(user)
   end
+  
+  # 検索方法の分岐を定義
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+  end  
 end
